c1adf04c212ce96f2c26e2bc118645908a33966f
v.shilin <v.shilin@centrexpress.ru>
2018-10-16 15:56:28 +0300

    Добавлено подтверждение на удаление

--- src/components/cardItem.jsx
+++ src/components/cardItem.jsx
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
-import { List, Card, Upload, Icon, message, Tooltip } from 'antd';
-import Lightbox from '../lib/index'; 
+import { List, Card, Upload, Icon, message, Tooltip, Modal } from 'antd';
+import Lightbox from '../lib/index';
 
 const { Meta } = Card;
+const { confirm } = Modal;
 
 const showMessage = () => {
     message.success('Файл успешно добавлен!');
@@ -31,58 +32,75 @@ class CardItem extends Component {
     }
 
     showSlider() {
-        const { item, galleryActions } = this.props;              
+        const { item, galleryActions } = this.props;
         galleryActions.toggleLightbox(item);
-    }    
-  
+    }
+
+    showDeleteConfirm() {
+        const { item, thingsActions } = this.props;
+        confirm({
+            centered: true,
+            title: 'Удалить?',
+            content: '',
+            okText: 'Да',
+            okType: 'danger',
+            cancelText: 'Нет',
+            onOk() {
+                console.log('OK');
+                thingsActions.fetchRemove(item);
+            },
+            onCancel() {
+                console.log('Cancel');
+            },
+        });
+    }
 
     render() {
         const { item, gallery, galleryActions, thingsActions } = this.props;
         const show = (item === gallery.item) && (item.urls);
 
-        const defaultUrl = (item.urls) ? Object.values(item.urls)[0].url : 'https://firebasestorage.googleapis.com/v0/b/ordering-things-api.appspot.com/o/default%2Fempty.jpg?alt=media&token=427e09b3-98ac-4967-b058-74c99a039f86' ;
+        const defaultUrl = (item.urls) ? Object.values(item.urls)[0].url : 'https://firebasestorage.googleapis.com/v0/b/ordering-things-api.appspot.com/o/default%2Fempty.jpg?alt=media&token=427e09b3-98ac-4967-b058-74c99a039f86';
 
         const imageUrls = (item.urls) ? Object.values(item.urls)
-            .map((urlObj, i) =>	                 
-            {
+            .map((urlObj, i) => {
                 return {
                     src: urlObj.url,
                     title: item.title,
-                    description: `Фото ${i + 1}/${Object.values(item.urls).length}`                               
+                    description: `Фото ${i + 1}/${Object.values(item.urls).length}`
                 };
             }) : null;
 
         return (
-            <div>    
+            <div>
                 <List.Item>
                     <Card
                         actions={
                             [
-                                <Upload 
+                                <Upload
                                     accept="image/*"
                                     name="fileAdd"
                                     showUploadList={false}
                                     beforeUpload={this.beforeUpload.bind(this)}
                                 >
-                                    
+
                                     <Tooltip placement="bottomLeft" title="Добавить фото">
-                                        <Icon type="file-add" /> 
+                                        <Icon type="file-add" />
                                     </Tooltip>
                                 </Upload>,
 
                                 <Tooltip placement="bottomLeft" title="Изменить">
-                                    <Icon type="edit" /> 
+                                    <Icon type="edit" />
                                 </Tooltip>,
 
                                 <Tooltip placement="bottomLeft" title="Удалить">
-                                    <Icon type="delete" onClick={() => {thingsActions.fetchRemove(item)}} /> 
-                                </Tooltip>                            
+                                    <Icon type="delete" onClick={this.showDeleteConfirm.bind(this)} />
+                                </Tooltip>
                             ]}
                         hoverable
                         style={{ width: 180 }}
                         cover={<img alt="example" src={defaultUrl} onClick={this.showSlider.bind(this)} />}>
                         <Meta
-                            title={item.title}/>
+                            title={item.title} />
                     </Card>
                 </List.Item>
 
